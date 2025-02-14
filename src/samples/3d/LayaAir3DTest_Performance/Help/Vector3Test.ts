cd195706b78026946d6f4f121aca511e96accc6e
zgsxof <375094497@qq.com>
2025-02-14 13:43:47 +0800

    Delete src/samples directory

--- src/samples/3d/LayaAir3DTest_Performance/Help/Vector3Test.ts
+++
@@ -1,8 +0,0 @@
-export class Vector3Test {
-	x: number = 0;
-	y: number = 0;
-	z: number = 0;
-	w: number = 0;
-}
-
-
--- src/samples/3d/LayaAir3DTest_Performance/Help/Vector3TestFloatArray.ts
+++
@@ -1,6 +0,0 @@
-
-export class Vector3TestFloatArray {
-	e: Float32Array = new Float32Array(4);
-}
-
-
